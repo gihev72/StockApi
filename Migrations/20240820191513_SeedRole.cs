040e45e9781a6751f6becbb3c93dddb5317eb00f
mohsen <ahmadi.mohsen1714@gmail.com>
2024-08-21 20:50:42 +0300

    add login feature

---
+++ Migrations/20240820191513_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "08b51e58-bab5-4e61-94de-4654d127046e", null, "Admin", "ADMIN" },
+                    { "482529df-f68b-4a9c-b8d5-9a5c3e838499", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "08b51e58-bab5-4e61-94de-4654d127046e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "482529df-f68b-4a9c-b8d5-9a5c3e838499");
+        }
+    }
+}
